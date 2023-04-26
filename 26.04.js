0b7d7dbae11f4892092aff5d901e19ec0a723439
Jamile Moreira <jamilemoreira2310@gmail.com>
2023-04-26 19:44:28 -0300

    anotacoes do dia

--- 26.04.js
+++ 26.04.js
@@ -91,7 +91,7 @@ usuarios.sort((primeiro, segundo) => {
 
 console.log(usuarios);
 
-// Ordenacao de array con strings 
+// Ordenacao de array con strings - letras minusculas e sem acentos 
 
 const pessoas = ['joao', 'ana', 'carlos', 'beatriz'];
 
@@ -99,4 +99,57 @@ pessoas.sort();
 
 console.log(pessoas);
 
-// 
+// exemplo de ordenacao string crescente e decrescente - usando metodo localeCompare()
+//-CRESCENTE
+const pessoas = ['Joao', 'ana', 'carlos', 'beatriz'];
+
+pessoas.sort((a, b) => {
+    return a.localeCompare(b);
+});
+
+console.log(pessoas);
+
+//-DECESCENTE
+const pessoas = ['Joao', 'ana', 'carlos', 'beatriz'];
+
+pessoas.sort((a, b) => {
+    return b.localeCompare(a);
+});
+
+console.log(pessoas);
+
+// Dado o array de carros, crie uma funcao que receba dois argumentos: o array e o campo a ser ordenado. 
+
+const carros = [
+    {nome: 'corola', marca:'toyota', ano: '2020', cor: 'prata'},
+    {nome: 'argo', marca:'fiat', ano: '2021', cor: 'preto'},
+    {nome: 'ranger', marca:'ford', ano: '2021', cor: 'prata'},
+    {nome: 'hilux', marca:'toyota', ano: '2018', cor: 'branco'}
+];
+
+const ordenarCarros = (arrayCarros,campoOrdenacao) => {
+    arrayCarros.sort((primeiro, segundo) => {
+        return primeiro[campoOrdenacao].localeCompare(segundo[campoOrdenacao]);
+    });
+    console.log(arrayCarros)
+}
+ordenarCarros(carros,'marca');
+
+//somar os elementos usando for
+
+const array = [0, 1, 2, 3, 4];
+
+let somaTotal = array[0]
+for (let i = 1; i < array.length; i++) {
+    const elementoAtual = array[i];
+    somaTotal = somaTotal + elementoAtual;
+}
+console.log(somaTotal)
+
+// exemplo de refaturacao do codigo anterior, usando reduce()
+
+const array = [0, 1, 2, 3, 4];
+
+array.reduce((acumulador, valorAtual, indice, array) => {
+    return acumulador + valorAtual
+});
