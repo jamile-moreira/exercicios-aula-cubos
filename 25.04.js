d41ef804384b03d5e6bd5b997babd1c69ca14d94
Jamile Moreira <jamilemoreira2310@gmail.com>
2023-04-25 22:56:39 -0300

    anotacoes de aula

--- 25.04.js
+++ 25.04.js
@@ -100,3 +100,126 @@ const fiscalizarFesta = (arrayObjetos) => {
     }
 }
 fiscalizarFesta(usuarios);
+
+// exemoplo do uso do metodo some()
+
+const nomes = ['joao','maria', 'jose', 'rodrigo'];
+
+const minhaFuncaoCallback = (nome) => {
+    return nome === 'joao';
+}
+const resultado = nomes.some(minhaFuncaoCallback);
+
+console.log(resultado);
+
+// OU
+
+const resultado = nomes.some((nome) => {
+    return nome === 'joao';
+});
+
+console.log(resultado);
+
+// Criar uma funcao que percorra um array com nomes de arquivos. A funcao deve verificar se existe ao menos um arquivo com a entensao '.bat'
+
+const arquivos = [
+    'teste.txt',
+    'foto.png',
+    'contrato.doc',
+    'instalador.exe'
+];
+
+const antiVirus = (arrayArquivos) => {
+    const resultado = arrayArquivos.some((arquivo) => {
+        const existeExtensao = arquivo.indexOf('.bat');
+
+        return existeExtensao !== -1;
+    });
+
+    if (resultado) {
+        console.log('Virus detectado!')
+    } else{
+        console.log('Nenhum virus encontrado');
+    }
+}
+antiVirus(arquivos);
+
+// uso do metodo find()
+
+const usuarios = [
+    {nome: 'joao', idade: 23},
+    {nome: 'maria', idade: 18},
+    {nome: 'ana', idade: 30},
+    {nome: 'rodrigo', idade: 17}
+];
+
+const resultado = usuarios.find((usuario) => {
+    return usuario.nome === 'jamile';
+});
+console.log(resultado);
+
+// Crie uma funcao que receba nome da marca de um carro e um array de objetos contendo as propriedades nome, marca, ano e cor. A funcao deve encontrar os dados do carro 
+
+const carros = [
+    {nome: 'corola', marca:'toyota', ano: '2020', cor: 'prata'},
+    {nome: 'argo', marca:'fiat', ano: '2021', cor: 'preto'},
+    {nome: 'ranger', marca:'ford', ano: '2021', cor: 'prata'},
+    {nome: 'hilux', marca:'toyota', ano: '2018', cor: 'branco'}
+];
+const buscarCarro = (marca, arrayCarros) => {
+    return arrayCarros.find((carro) => {
+        return carro.marca === marca;
+    });
+};
+const resultado = buscarCarro('toyota', carros);
+console.log(resultado);
+
+// exemplo de uso metodo findIndex
+
+const nomes = ['joao', 'ana', 'rodrigo'];
+const resultado = nomes.findIndex((nome) => {
+    return nome === 'jose';
+});
+
+console.log(resultado);
+
+// exemplo do uso do metodo filter()
+
+const numeros = [1, 4, 4, 5, 3, 1, 8];
+
+const resultado = numeros.filter((numero) => {
+    return numero === 3;
+})
+
+console.log(resultado);
+
+// exemplo de uso do metodo map()
+
+const usuarios = [
+    { nome: 'Guido', sobrenome: 'Cerqueira', idade: 31},
+    { nome: 'Daniel', sobrenome: 'Lopes', idade: 29},
+    { nome: 'Vitor', sobrenome: 'Vidal', idade: 28}
+]
+const novoArray = usuarios.map((usuario) => {
+    return{
+    nomeCompleto: `${usuario.nome} ${usuario.sobrenome}` , idade: usuario.idade
+    }    
+});
+console.log(novoArray);
+
+// Criar um novo array de produtos contendo nome, preco e desconto. Valor de desconto =10%
+
+const produtos = [
+    { nome: 'arroz', preco: 500},
+    { nome: 'carne', preco: 3200},
+    { nome: 'biscoito', preco: 450},
+    { nome: 'banana', preco: 320}   
+]
+const produtosDescontos = produtos.map((produto) => {
+    return {
+        nome: produto.nome,
+        preco: produto.preco,
+        desconto: produto.preco * 0.1 
+    }
+});
+console.log(produtosDescontos);
