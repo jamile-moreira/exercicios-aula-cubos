b00b059122c99d449dcd78baaa422af17eeb17fa
Jamile Moreira <jamilemoreira2310@gmail.com>
2023-08-23 16:35:56 -0300

    exercicio ao vivo, até item 3

---
+++ React-router/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+// Reaproveitamento de estrutura
+import { Outlet } from 'react-router-dom'
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>React Router</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ React-router/vite-project/src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// Configurar router
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Contact from './routes/Contact.jsx'
+import ErrorPage from './routes/ErrorPage.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Home />,
+//   },
+//   {
+//     path: 'contact',
+//     element: <Contact />,
+//   },
+// ])
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    //Página de erro
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
